7f253f23f8305b0f50977bccc1e2cd507e52dfc8
devyubi <dev.munyubi@gmail.com>
2025-08-13 14:22:45 +0900

    [docs] context api의 이해

--- src/components/todos/TodoList.tsx
+++ src/components/todos/TodoList.tsx
@@ -1,36 +1,20 @@
+import { useTodoState } from '../../context/todo/hooks';
 import TodoItem from './TodoItem';
-import { TodoType } from '../../types/Todotypes';
 
-interface TodoListProps {
-  todos: TodoType[];
-  onToggle: (id: string) => void;
-  onDelete: (id: string) => void;
-  onEdit: () => void;
-}
-
-const TodoList = ({ todos, onToggle, onDelete, onEdit }: TodoListProps): JSX.Element => {
-  //ts
-
-  //tsx
+const TodoList = (): JSX.Element => {
+  const { todos } = useTodoState();
   return (
     <div>
-      <h2>할일 목록</h2>
+      <h2>할일목록</h2>
       {todos.length === 0 ? (
         <p>목록이 없습니다.</p>
       ) : (
         <ul>
           {todos.map(item => (
-            <TodoItem
-              key={item.id}
-              todo={item}
-              onToggle={onToggle}
-              onDelete={onDelete}
-              onEdit={onEdit}
-            />
+            <TodoItem key={item.id} todo={item} />
           ))}
         </ul>
       )}
-      {/* 할일 즉, todos 는 여러개의 item으로 구성된 배열임. map으로 출력함 */}
     </div>
   );
 };
--- src/components/todos/TodoWrite.tsx
+++ src/components/todos/TodoWrite.tsx
@@ -1,12 +1,42 @@
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { TodoType } from '../../types/Todotypes';
+import { useTodoActions } from '../../context/todo/hooks';
 
-type TodoWriteProps = {
-  setTodos: React.Dispatch<React.SetStateAction<TodoType[]>>;
-  handleTodoUpdate: () => void;
-};
+const TodoWrite = () => {
+  // js 자리
+  const { addTodo } = useTodoActions();
+  // 할일 제목 값 관리
+  const [title, setTitle] = useState<string>('');
+  // title 변경시 onChange 이벤트 처리해보기
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    // enter 키를 입력시 처리
+    if (e.key === 'Enter') {
+      handleAdd();
+    }
+  };
 
-const TodoWrite = ({ setTodos, handleTodoUpdate }: TodoWriteProps) => {
-  return <div>TodoWrite</div>;
+  // 새 할일 등록하기
+  const handleAdd = () => {
+    if (title.trim()) {
+      const newTodo: TodoType = {
+        id: Date.now().toString(),
+        title: title,
+        completed: false,
+      };
+      addTodo(newTodo);
+      setTitle('');
+    }
+  };
+  // jsx 자리
+  return (
+    <div>
+      <input type="text" value={title} onChange={e => handleChange(e)} onKeyDown={handleKeyDown} />
+      <button onClick={handleAdd}>등록</button>
+    </div>
+  );
 };
 
 export default TodoWrite;
